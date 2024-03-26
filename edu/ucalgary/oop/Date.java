5c55f9acf42ab6fcc148fa11da7c4b105332a5ca
brandonnguyen3 <brandon.nguyen3@ucalgary.ca>
2024-03-26 16:16:29 -0600

    Modifications made to Date. Have not tested yet.

--- edu/ucalgary/oop/Date.java
+++ edu/ucalgary/oop/Date.java
@@ -1,7 +1,9 @@
-/*
- * Author: Brandon Nguyen
- * Date: 03/12/2024
- */
+/**
+@author Brandon Nguyen <a
+@version 1.1
+*/
+
+
 package edu.ucalgary.oop;
 
 import java.time.LocalDate;
@@ -11,56 +13,29 @@ import java.time.format.DateTimeFormatter;
 Date class which MedicalRecord, ReliefService, and DisasterVictim will inherit from.
  */
 class Date {
-    private int day;
-    private int month;
-    private int year;
+    //Store a value of date as a string. 
+    private String date;
 
     // Constructor
-    public Date(int day, int month, int year) {
-        this.day = day;
-        this.month = month;
-        this.year = year;
+    public Date(String date) {
+        if (!isValidDateFormat(date)) {
+            throw new IllegalArgumentException("Invalid date format for treatment details. Expected format: YYYY-MM-DD");
+        }
+        this.date = date;
     }
 
-    // Getters and setters
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
 
     /*setDate is used to turn the string into integers and pass those in the setters.  */
     public void setDate(String date) {
         if (!isValidDateFormat(date)) {
             throw new IllegalArgumentException("Invalid date format. Expected format: YYYY-MM-DD");
         } 
-        String[] parts = date.split("-");
-        this.year = Integer.parseInt(parts[0]);
-        this.month = Integer.parseInt(parts[1]);
-        this.day = Integer.parseInt(parts[2]);
+        this.date = date;
     }
 
     /*getDate retrieves the entire date and returns a string with the parts.*/
     public String getDate() {
-        return String.format("%04d-%02d-%02d", year, month, day);
+        return this.date;
     }
 
     //used to check that it is in the format of YYYY-MM-DD
