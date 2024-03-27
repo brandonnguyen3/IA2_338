bc08614a5741d6d32ed2a620bf3c31c3d2df6509
brandonnguyen3 <brandon.nguyen3@ucalgary.ca>
2024-03-26 18:06:20 -0600

    ReliefService Inheritance done

--- edu/ucalgary/oop/ReliefService.java
+++ edu/ucalgary/oop/ReliefService.java
@@ -1,92 +1,84 @@
 /**
 @author Brandon Nguyen
-@version 1.0
+@version 1.1
  */
 
-package edu.ucalgary.oop;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+/*
+ * Modifications: (03/26)
+ * Got rid of dateOfInquiry. Now inherits it from Date.
+ * Got rid of Date Helper function
+ */
 
-public class ReliefService {
-    private Inquirer inquirer;
-    private DisasterVictim missingPerson;
-    private String dateOfInquiry;
-    private String infoProvided;
-    private Location lastKnownLocation;
+ package edu.ucalgary.oop;
 
-    // Constructor
-    public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
+ 
+ public class ReliefService extends Date {
+     private Inquirer inquirer;
+     private DisasterVictim missingPerson;
+     
+     private String infoProvided;
+     private Location lastKnownLocation;
+ 
+     // Constructor
+     public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
+        super(dateOfInquiry); 
         this.inquirer = inquirer;
-        this.missingPerson = missingPerson;
-        setDateOfInquiry(dateOfInquiry);
-        this.infoProvided = infoProvided;
-        this.lastKnownLocation = lastKnownLocation;
-    }
-
-    // Getter and setter for inquirer
-    public Inquirer getInquirer() {
-        return inquirer;
-    }
-
-    public void setInquirer(Inquirer inquirer) {
-        this.inquirer = inquirer;
-    }
-
-    // Getter and setter for missingPerson
-    public DisasterVictim getMissingPerson() {
-        return missingPerson;
-    }
-
-    public void setMissingPerson(DisasterVictim missingPerson) {
-        this.missingPerson = missingPerson;
-    }
-
-    // Getter and setter for dateOfInquiry
-    public String getDateOfInquiry() {
-        return dateOfInquiry;
-    }
-
-    public void setDateOfInquiry(String dateOfInquiry) {
-        // Check if the dateOfInquiry string matches the expected date format
-        if (!isValidDateFormat(dateOfInquiry)) {
-            throw new IllegalArgumentException("Invalid date format for date of inquiry. Expected format: YYYY-MM-DD");
-        }
-        this.dateOfInquiry = dateOfInquiry;
-    }
-
-    // Getter and setter for infoProvided
-    public String getInfoProvided() {
-        return infoProvided;
-    }
-
-    public void setInfoProvided(String infoProvided) {
-        this.infoProvided = infoProvided;
-    }
-
-    // Getter and setter for lastKnownLocation
-    public Location getLastKnownLocation() {
-        return lastKnownLocation;
-    }
-
-    public void setLastKnownLocation(Location lastKnownLocation) {
-        this.lastKnownLocation = lastKnownLocation;
-    }
-
-    // Helper method to check if a string matches the YYYY-MM-DD date format
-    private boolean isValidDateFormat(String date) {
-        try {
-            LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-    public String getLogDetails() {
-       return "Inquirer: " + inquirer.getFirstName() + 
-           ", Missing Person: " + missingPerson.getFirstName() + 
-           ", Date of Inquiry: " + dateOfInquiry + 
-           ", Info Provided: " + infoProvided + 
-           ", Last Known Location: " + lastKnownLocation.getName();
-    }
-    
-}
+         this.missingPerson = missingPerson;
+         this.infoProvided = infoProvided;
+         this.lastKnownLocation = lastKnownLocation;
+     }
+ 
+     // Getter and setter for inquirer
+     public Inquirer getInquirer() {
+         return inquirer;
+     }
+ 
+     public void setInquirer(Inquirer inquirer) {
+         this.inquirer = inquirer;
+     }
+ 
+     // Getter and setter for missingPerson
+     public DisasterVictim getMissingPerson() {
+         return missingPerson;
+     }
+ 
+     public void setMissingPerson(DisasterVictim missingPerson) {
+         this.missingPerson = missingPerson;
+     }
+ 
+     // Getter and setter for dateOfInquiry
+     public String getDateOfInquiry() {
+        return super.getDate();
+     }
+ 
+     public void setDateOfInquiry(String dateOfInquiry) {
+        super.setDate(dateOfInquiry);
+     }
+ 
+     // Getter and setter for infoProvided
+     public String getInfoProvided() {
+         return infoProvided;
+     }
+ 
+     public void setInfoProvided(String infoProvided) {
+         this.infoProvided = infoProvided;
+     }
+ 
+     // Getter and setter for lastKnownLocation
+     public Location getLastKnownLocation() {
+         return lastKnownLocation;
+     }
+ 
+     public void setLastKnownLocation(Location lastKnownLocation) {
+         this.lastKnownLocation = lastKnownLocation;
+     }
+ 
+     public String getLogDetails() {
+        return "Inquirer: " + inquirer.getFirstName() + 
+            ", Missing Person: " + missingPerson.getFirstName() + 
+            ", Date of Inquiry: " + getDateOfInquiry() + 
+            ", Info Provided: " + infoProvided + 
+            ", Last Known Location: " + lastKnownLocation.getName();
+     }
+     
+ }
